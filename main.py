150a38480dd14374ee2565cc737bf6d16925ec6c
pmiko <pio.mikolajczyk@gmail.com>
2024-12-02 17:43:56 +0100

    added fft metric

--- main.py
+++ main.py
@@ -27,6 +27,15 @@ base = 'C:\PRACA\Depi\Prg_prj\Dicom_Evaluator\Dicom_Evaluator\CALIBRATION_IMG\\'
 folder_1 = 'fotki_do_kalibracjI\\'
 folder_2 = 'testy_do_kalibracji_obrazek_testowy\\'
 
+class AddNoise:
+    def __init__(self, mean=0.0, std=0.1):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        noise = torch.randn(tensor.size()) * self.std + self.mean
+        return tensor + noise
+
 start = time.time()
 def show_all_pic(images, disp):
     if disp == 1:
@@ -73,6 +82,7 @@ def image_avg(images):
     return avg_image
 
 
+
 # Note: Import DCM
 def zips2dcm(base,path):
     path_f = base+path
@@ -101,31 +111,94 @@ def read_dcm(ph,f,channel='col',disp=0):
 # Note: Brisque method
 # Attention: This score should be from 0 to 100 where 0 is best perceived image
 brisque = BRISQUE(url=False)
-imgs = read_dcm(ph1, f1[10],channel='col',disp=1)
+
 # Note: Brisque method
 
 # Note: ARNIQA method
 # print(torch.__version__)
 model = h.load(repo_or_dir="miccunifi/ARNIQA", source="github", model="ARNIQA",
-                       regressor_dataset="kadid10k")
+                       regressor_dataset="kadid10k").to(device)
+model.eval()
 preprocess = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    #AddNoise(0.,0.1),
+
 ])
 # print(model)
 # Note: ARNIQA method
-model.eval().to(device)
-for img in imgs:
-    # print(brisque.score(img= img))
-    im1 = preprocess(img).float().to(device)
-    im1 = im1.unsqueeze(0)
-    im2 = preprocess(img).float().to(device)
-    im2 = im2.unsqueeze(0)
-    # img_ds = transforms.Resize((im.shape[1] // 2, im.shape[0] // 2))(im).float()
-    #with torch.no_grad(), torch.cuda.amp.autocast():
-    ARNIQA_score = model(im1,im2, return_embedding=False, scale_score=True)
-    print("ARNIQA score: ",ARNIQA_score)
+
+# Note: Frequency distribution metric
+def freq_diversity_metric(img,impre1,impre2,n_regions=20):
+    im = torch.from_numpy(img).float().to(device)
+    fft_im = torch.fft.fftn(im)
+    fft_imp1 = torch.fft.fftn(impre1.permute(1,2,0))
+    fft_imp2 = torch.fft.fftn(impre2.permute(1,2,0))
+    fft_shift_im = torch.fft.fftshift(fft_im)
+    fft_shift_imp1 = torch.fft.fftshift(fft_imp1)
+    fft_shift_imp2 = torch.fft.fftshift(fft_imp2)
+    spectrum = torch.abs(fft_shift_im)
+    spectrum_p1 = torch.abs(fft_shift_imp1)
+    spectrum_p2 = torch.abs(fft_shift_imp2)
+    height, width = spectrum.shape[-3], spectrum.shape[-2]
+    region_height = height // n_regions
+    region_width = width // n_regions
+    region_energies = []
+    region_energies_p1 = []
+    region_energies_p2 = []
+
+    for i in range(n_regions):
+        for j in range(n_regions):
+            start_h = i * region_height
+            end_h = (i + 1) * region_height
+            start_w = j * region_width
+            end_w = (j + 1) * region_width
+            region_energy = torch.mean(spectrum[start_h:end_h, start_w:end_w,:])
+            region_energy_p1 = torch.mean(spectrum_p1[start_h:end_h, start_w:end_w,:])
+            region_energy_p2 = torch.mean(spectrum_p2[start_h:end_h, start_w:end_w,:])
+            region_energies.append(region_energy)
+            region_energies_p1.append(region_energy_p1)
+            region_energies_p2.append(region_energy_p2)
+
+
+    for i in range(len(region_energies)):
+        for j in range(i + 1, len(region_energies)):
+            diversity_score = 1/( torch.mean(
+                torch.var(torch.cat([
+                torch.abs(region_energies[i] + region_energies[j]).unsqueeze(0),
+                torch.abs(region_energies_p1[i] + region_energies_p1[j]).unsqueeze(0),
+                torch.abs(region_energies_p2[i] + region_energies_p2[j]).unsqueeze(0)], dim=0)))+1e-6)
+
+    return diversity_score
+
+# Note: Frequency distribution metric
+ARNIQA_scores = []
+fft_diversity_scores = []
+imgs = read_dcm(ph2, f2[20],channel='col',disp=1)
+i = 0
+with torch.no_grad():
+    for img in imgs:
+        print("Image Number: ", i+1,"\n>------------------------------>")
+        # print(brisque.score(img= img))
+        im1 = preprocess(img).float().to(device)
+        im2 = preprocess(img).float().to(device)
+
+        freq_div_Score = freq_diversity_metric(img, im1, im2)
+        fft_diversity_scores.append(freq_div_Score)
+        print("Freq Diversity Score: ", freq_div_Score)
+
+        im1 = im1.unsqueeze(0)
+        im2 = im2.unsqueeze(0)
+        ARNIQA_score = model(im1,im2, return_embedding=False, scale_score=True)
+        ARNIQA_scores.append(ARNIQA_score)
+        print("ARNIQA score: ",ARNIQA_score, "\n<------------------------------<\n")
+        i+=1
+
+
+
 
 end = time.time()
-print("Evaluation time: ", end - start)
+print("Evaluation time: ", round(end - start,2) , " [s]")
+print("Best Image Number ARNIQA:",torch.argmax(torch.FloatTensor(ARNIQA_scores)).item())
+print("Best Image Number FREQ DIV:",torch.argmax(torch.FloatTensor(fft_diversity_scores)).item())
 
