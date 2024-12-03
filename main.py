1751025bfe175a5333c4784f1b0871be72a41df5
pmiko <pio.mikolajczyk@gmail.com>
2024-12-03 17:52:03 +0100

    added more metrics

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib
 import cv2 as cv
 import torch
+import torcheval
+import torchvision
+from ignite.engine import Engine
+from ignite.metrics import PSNR
 from skimage.feature import match_template, peak_local_max
 from skimage.transform import rescale, resize, downscale_local_mean
 from matplotlib import pyplot as plt
@@ -26,6 +30,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 base = 'C:\PRACA\Depi\Prg_prj\Dicom_Evaluator\Dicom_Evaluator\CALIBRATION_IMG\\'
 folder_1 = 'fotki_do_kalibracjI\\'
 folder_2 = 'testy_do_kalibracji_obrazek_testowy\\'
+folder_3 = 'fotki_do_kalibracji_NANOCHECKER\\'
 
 class AddNoise:
     def __init__(self, mean=0.0, std=0.1):
@@ -97,10 +102,8 @@ def zips2dcm(base,path):
             os.remove(file_name)
         return path_f, [item[0:-4]+'.dcm' for item in  fn]
     else: return path_f, [item for item in items_p1 if item.endswith('.dcm') and os.path.isfile(os.path.join(path_f, item))]
-ph1,f1 = zips2dcm(base,folder_1)
-ph2,f2 = zips2dcm(base,folder_2)
-# Note: Import DCM
-disp = 1
+
+
 
 def read_dcm(ph,f,channel='col',disp=0):
     filename = pydicom.data.data_manager.get_files(ph, f)[0]
@@ -110,9 +113,17 @@ def read_dcm(ph,f,channel='col',disp=0):
 
 # Note: Brisque method
 # Attention: This score should be from 0 to 100 where 0 is best perceived image
-brisque = BRISQUE(url=False)
 
 # Note: Brisque method
+brisque = BRISQUE(url=False)
+def Brisque_score(img,impre1,impre2):
+    img_score = torch.tensor(brisque.score(img=img)).unsqueeze(0)
+    impre1 = torchvision.transforms.functional.to_pil_image(noise_addition(impre1))
+    impre2 = torchvision.transforms.functional.to_pil_image(impre2)
+    imgp1_score = torch.tensor(brisque.score(img=impre1)).unsqueeze(0)
+    imgp2_score = torch.tensor(brisque.score(img=impre2)).unsqueeze(0)
+    b_scores = 1 / torch.std(torch.cat([img_score,imgp1_score, imgp2_score], dim=0))
+    return b_scores
 
 # Note: ARNIQA method
 # print(torch.__version__)
@@ -125,6 +136,12 @@ preprocess = transforms.Compose([
     #AddNoise(0.,0.1),
 
 ])
+
+noise_addition = transforms.Compose([
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    AddNoise(0.,0.2),
+
+])
 # print(model)
 # Note: ARNIQA method
 
@@ -132,7 +149,7 @@ preprocess = transforms.Compose([
 def freq_diversity_metric(img,impre1,impre2,n_regions=20):
     im = torch.from_numpy(img).float().to(device)
     fft_im = torch.fft.fftn(im)
-    fft_imp1 = torch.fft.fftn(impre1.permute(1,2,0))
+    fft_imp1 = torch.fft.fftn(noise_addition(impre1).permute(1,2,0))
     fft_imp2 = torch.fft.fftn(impre2.permute(1,2,0))
     fft_shift_im = torch.fft.fftshift(fft_im)
     fft_shift_imp1 = torch.fft.fftshift(fft_imp1)
@@ -172,33 +189,72 @@ def freq_diversity_metric(img,impre1,impre2,n_regions=20):
     return diversity_score
 
 # Note: Frequency distribution metric
+
+# Note: Gradient similarity metric
+def grad_difference_metric(img, impre1, impre2, n_regions=20):
+    im = torch.from_numpy(img).float().to(device)
+    imp1=noise_addition(impre1).permute(1, 2, 0)
+    imp2=impre2.permute(1, 2, 0)
+
+    _,grad_im = torch.gradient(im,dim=[0,1])
+    _,grad_imp1 = torch.gradient(imp1,dim=[0,1])
+    _,grad_imp2 = torch.gradient(imp2,dim=[0,1])
+    grad_mse = torch.nn.functional.mse_loss(grad_im,grad_imp1) + torch.nn.functional.mse_loss(grad_im,grad_imp2)
+    grad_div_score = grad_mse.mean()
+    return grad_div_score
+
+# Note: Gradient similarity metric
+
+# Note: PSNR metric
+
+# Note: PSNR metric
+
+
+ph,f = zips2dcm(base,folder_2)
+# Note: Import DCM
+disp = 1
+psnr_scores = []
+BRISQUE_scores = []
 ARNIQA_scores = []
 fft_diversity_scores = []
-imgs = read_dcm(ph2, f2[20],channel='col',disp=1)
+grad_diff_scores = []
+imgs = read_dcm(ph, f[12],channel='col',disp=1)
 i = 0
 with torch.no_grad():
     for img in imgs:
         print("Image Number: ", i+1,"\n>------------------------------>")
-        # print(brisque.score(img= img))
         im1 = preprocess(img).float().to(device)
         im2 = preprocess(img).float().to(device)
+
+        psnr_score = torcheval.metrics.PeakSignalNoiseRatio(torch.from_numpy(img).float().to(device))
+        psnr_scores.append(psnr_score)
+        print("PSNR Score          : ", psnr_score)
+
+        BRISQUE_score = Brisque_score(img, im1, im2)
+        BRISQUE_scores.append(BRISQUE_score)
+        print("BRISQUE Score       : ", BRISQUE_score)
 
         freq_div_Score = freq_diversity_metric(img, im1, im2)
         fft_diversity_scores.append(freq_div_Score)
         print("Freq Diversity Score: ", freq_div_Score)
 
+        grad_div_Score = grad_difference_metric(img, im1, im2)
+        grad_diff_scores.append(grad_div_Score)
+        print("Grad Diversity Score: ", grad_div_Score)
+
         im1 = im1.unsqueeze(0)
         im2 = im2.unsqueeze(0)
         ARNIQA_score = model(im1,im2, return_embedding=False, scale_score=True)
         ARNIQA_scores.append(ARNIQA_score)
-        print("ARNIQA score: ",ARNIQA_score, "\n<------------------------------<\n")
+        print("ARNIQA Score        : ",ARNIQA_score, "\n<------------------------------<\n")
         i+=1
-
-
 
 
 end = time.time()
 print("Evaluation time: ", round(end - start,2) , " [s]")
+print("Best Image Number BRISQUE:",torch.argmax(torch.FloatTensor(BRISQUE_scores)).item())
 print("Best Image Number ARNIQA:",torch.argmax(torch.FloatTensor(ARNIQA_scores)).item())
 print("Best Image Number FREQ DIV:",torch.argmax(torch.FloatTensor(fft_diversity_scores)).item())
+print("Best Image Number GRAD DIFF:",torch.argmax(torch.FloatTensor(grad_diff_scores)).item())
+print("Best Image Number PSNR:",torch.argmax(torch.FloatTensor(psnr_scores)).item())
 
